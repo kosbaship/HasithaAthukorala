adf201c0c3b41fb9c1990949eb14328b2c5f0bc4
mohammed hashish <mhashish@smart-medicalservices.com>
2022-01-02 10:02:37 +0200

    Initial commit

---
+++ lib/screens/home/presentation/blocs/log_out/log_out_state.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+@immutable
+abstract class LogOutState extends Equatable {
+  LogOutState([List props = const <dynamic>[]]) : super(props);
+}
+
+class LoggedInInitialState extends LogOutState {}
+
+class LoggedInState extends LogOutState {}
+
+class LoggedOutState extends LogOutState {}
+
+class LoadingState extends LogOutState {}
+
+class ErrorState extends LogOutState {
+  final String message;
+
+  ErrorState(this.message);
+}
